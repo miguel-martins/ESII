7b36684ff491a8238589635d7184ef48838823e0
jose reis <jvpreis@gmail.com>
2020-03-01 11:19:17 +0000

    Javadoc

--- Git-Maven-Jenkins/src/main/java/Exemplo_ESII/Git_Maven_Jenkins/App.java
+++ Git-Maven-Jenkins/src/main/java/Exemplo_ESII/Git_Maven_Jenkins/App.java
@@ -1,8 +1,8 @@
 package Exemplo_ESII.Git_Maven_Jenkins;
 
 /**
- * Hello world!
- *
+ * Simple example for Software Engineering II
+ *@author José Reis
  */
 public class App 
 {
@@ -11,17 +11,30 @@ public class App
         System.out.println( "Código para testes ES II" );
     }
     
-    
+    /**
+     * Calculates the square of a number
+     * @param x the incoming number
+     * @return the Square of the number
+     */
     public int square(int x) {
 		return x * x;
 	}
 	
-    
+    /**
+     * Compares two strings lexicographically
+     * @param s1 the first string
+     * @param s2 the second string
+     * @return the value 0 if the argument string is equal to this string; a value less than 0 if this string is lexicographically less than the string argument; and a value greater than 0 if this string is lexicographically greater than the string argument.
+     */
 	public int compString(String s1,String s2) {
 		return (s1.compareTo(s2));
 	}
 
-	
+	/**
+     * Counts the number of chars "a" or "A" in a string
+     * @param palavra the string
+     * @return the number of "a" or "A" in the string
+     */
 	public int countA(String palavra) {
 		int count = 0;
 		for (int i = 0; i < palavra.length(); i++) {
